ca7bc0c722aa428224e47ecc06e7875798e5e8aa
ritesh singh <rootritesh64@gmail.com>
2020-11-17 10:15:40 +0530

    netflix-originaladded

---
+++ src/nf_original.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ns_original';
+
+function nf_ori(){
+    return(
+        <div className="nf-or">
+
+        <h1>Netflix Original</h1>
+        <p>Netflix is the home of amazing original programming that you can’t find anywhere else. Movies, TV shows, specials and more, it’s all tailored specifically to you. Watch now for free</p>
+        </div>
+    );
+}
