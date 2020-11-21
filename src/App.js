d15c6e910f7859f2f200fd9e95ebfd246ad5d7cb
ritesh singh <rootritesh64@gmail.com>
2020-11-21 21:18:49 +0530

    Bingeworthy tv shows added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './header/header';
 import Nf_ori from './nf_original/nf_original';
 import Pop from './popular_on_netflix/pop_nf';
 import Cf_tv from './child_family_tv/cf_tv';
+import Extv from './excited_tv/ex_tv';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Nf_ori/>
       <Pop/>
       <Cf_tv/>
+      <Extv/>
     </>  
   );
 }
--- src/excited_tv/data.js
+++ src/excited_tv/data.js
@@ -1,2 +1,60 @@
 const Data=[
     {
+        name: 'Narcos',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABa-oTyIxZjZX2RV59zIXh0I_NOJB16m7Iky-RTZ9jqavv58JOO4FcsoW_WH_vKdt9Cu8ZIZxDmDqb6FvEk6zj2aMp-GajzsrK9qJcFKdVZ7oNofBA9redABpGuwb.jpg?r=973',
+        link: 'https://www.netflix.com/in/title/80025172'
+    },
+    {
+        name: 'Designated Survivor',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUROuU7QYu8dTg9HDqhhNNn5DmImWUeMmUgCcmDkPM2gmo1ogpP7FxVJlOFG9ftwdTGB4s60zo_uLoHFxh7FQQk7DZ0sw-3iFg6SlII-X_-mba4HQL26OAEO6A8V.jpg?r=c5b',
+        link: 'https://www.netflix.com/in/title/80113647'
+    },
+    {
+        name: 'The Witcher',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYCXCfVkAFjsSQJGuFsXPb7ANJ4GKjDYZU85JaiQ-qDc1QJJCH_55dJJ1RWTdlo62fLFpBLMWrM4JunTyNQbGZm0_iKOwdU91su6fYovYxIWxgtI1KJzhdbWq8ta.jpg?r=382',
+        link: 'https://www.netflix.com/in/title/80189685'
+    },
+    {
+        name: 'Star Trek: Discovery',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT5q21jDh1FWHeCuDS53sHeXCzXeW_Snsty4N_1JypN_J-scsfXKqeNShJNd9rZkZZWFzg8ExYkM97JlGPp24WBtvgv6P5RPybAYOxLTz_drG_4e0qpzqFHogpAqW0SiYq0id14Mtmt__SxcbYdEsaJPxkLoXA.jpg?r=0e0',
+        link: 'https://www.netflix.com/in/title/80126024'
+    },
+    {
+        name: 'Cobra Kai',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdWxqiBcGxsB92JfOrQ011nc_kP3NjL07bbpLbF-HcKmiVykBvVgKnHRuJtEClOKxfSh52nnKZAY-icvnVK-WzX53tsKlJfQyILmSkZpPDgaWmQdDuuAwrGeyIzn.jpg?r=dd0',
+        link: 'https://www.netflix.com/in/title/81002370'
+    },
+    {
+        name: "Marvel's Iron Fist",
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRlxXgmZcTPEVLvCStvC6cYWuEmyulxMYbSeW4-vAmmrq2r2gnLqeyGL0gtNakVTjWrzKAnYqGvLCUAmB2H9bmhIb_0Sd3nIkBETtq3LW6sLBs_Zdp09teMHSr_3.jpg?r=712',
+        link: 'https://www.netflix.com/in/title/80002612'
+    },
+    {
+        name: "Marvel's Daredevil",
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABf1aIDwgezEmuaS1-DsEyBfCflPGm8mfaYXNO4xuJVNi2OIiTCQ8ez7KxMWTtwiWF7FTxmcxitn_gWpBabHn1SPqHbXVgOeQli4F8cAEDvz0wTd0aWm6jhn3yxSg.jpg?r=80f',
+        link: 'https://www.netflix.com/in/title/80018294'
+    },
+    {
+        name: 'The Last Kingdom',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABY61X8lNLH5LgEo88jfkptq8tUp-h5jF3x9Hqf9oLkIje28evOViDkydGLcwdufBglBSTNW6Ju8GrzvfsWiy8bXmqFj1H9DcPcHcEdbCBn6MNOWM5E7l5gKr4qfI.jpg?r=866',
+        link: 'https://www.netflix.com/in/title/80074249'
+    },
+    {
+        name: 'Outlander',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQOonpTIGdDQnC7BeDWTS8Q8D_IqvARGlgOjwe7NxHYPy4g89PM57NS5-sP_DtZ6YwxneB0lgWvwarrBQ9WxKrXCyjUzhHg5lpvOAQMryDuDoJnBM5LzCwIcNjr5.jpg?r=d09',
+        link: 'https://www.netflix.com/in/title/70285581'
+    },
+    {
+        name: 'BAKI',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABaN22ejU_36I_Xv514rFdeBjOTFPNtnuu0n8IZx4yp0oNgHwcB6FWU16kdR1geYDOxy-pxagxjdfFWISjJ6ZeNQ-4oux6Mkip5SeBApaQ-ii0uzuE1-nNgYc-8cG.jpg?r=37b',
+        link: 'https://www.netflix.com/in/title/80204451'
+    },
+    {
+        name: 'Blood of Zeus',
+        image: 'https://occ-0-4744-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdrPStH-yWlPhHfD9TglxN8Ut8CLO--JUpMKrCngDWuanUFoXSXTiUgf0sRIH1GsKbRfzyZc0hbTejdxAw1mCvst_8bKIfTdcjgiJbGTC_fN7R1absADL8f9w88C.jpg?r=058',
+        link: 'https://www.netflix.com/in/title/81001988'
+    },
+    
+]
+
+export default Data;
