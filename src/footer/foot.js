af1e80fffb298f6ea5446154c9d93d33c3329bb0
ritesh singh <rootritesh64@gmail.com>
2020-11-21 23:18:59 +0530

    footercompleted

--- src/footer/foot.js
+++ src/footer/foot.js
@@ -8,36 +8,37 @@ function Foote(){
                         {/* <li>Question ? Contact us.</li> */}
                 <div className="foot_div1">
                     <ul className ="foot_ul1">
-                        <li className ="content_li">FAQ</li>
-                        <li className ="content_li">Investor Relations</li>
-                        <li className ="content_li">Privacy</li>
-                        <li className ="content_li">Speed Test</li>
+                        <li className ="content_li"><a className="links" href="https://help.netflix.com/en/node/412">FAQ</a></li>
+                        <li className ="content_li"><a  className="links" href="http://ir.netflix.com/">
+                            Investor Relations</a></li>
+                        <li className ="content_li"><a className="links"  href="https://help.netflix.com/legal/privacy">Privacy</a></li>
+                        <li className ="content_li"><a  className="links"  href="https://fast.com/">Speed Test</a></li>
                     </ul>
                 </div>
 
                 <div className="foot_div2">
                     <ul className ="foot_ul2">
-                        <li  className ="content_li">Help Centre</li>
-                        <li className ="content_li">Jobs</li>
-                        <li className ="content_li">Cookie Preferences</li>
-                        <li className ="content_li">Legal Notices</li>
+                        <li  className ="content_li"><a  className="links" href="https://help.netflix.com/">Help Centre</a></li>
+                        <li className ="content_li"><a   className="links" href="https://jobs.netflix.com/jobs">Jobs</a></li>
+                        <li className ="content_li"><a  className="links" href="https://help.netflix.com/legal/privacy#cookies">Cookie Preferences</a></li>
+                        <li className ="content_li"><a  className="links" href="https://help.netflix.com/legal/notices">Legal Notices</a></li>
                     </ul>
                 </div>
 
                 <div className="foot_div3">
                     <ul className ="foot_ul3">
-                        <li className ="content_li">Account</li>
-                        <li className ="content_li">Ways to Watch </li>
-                        <li className ="content_li">Corporate Information</li>
-                        <li className ="content_li">Netflix Originals</li>
+                        <li className ="content_li"><a  className="links" href="https://www.netflix.com/youraccount">Account</a></li>
+                        <li className ="content_li"><a  className="links" href="https://www.netflix.com/watch">Ways to Watch </a></li>
+                        <li className ="content_li"><a  className="links" href="https://help.netflix.com/legal/corpinfo">Corporate Information</a></li>
+                        <li className ="content_li"><a  className="links" href="https://www.netflix.com/in/browse/genre/839338">Netflix Originals</a></li>
                     </ul>
                 </div>
 
                 <div className="foot_div4">
                     <ul className ="foot_ul4">
-                        <li className ="content_li">Media Centre</li>
-                        <li className ="content_li">Terms of Use</li>
-                        <li className ="content_li">Contact Us</li>
+                        <li className ="content_li"><a  className="links" href="https://media.netflix.com/">Media Centre</a></li>
+                        <li className ="content_li"><a  className="links" href="https://help.netflix.com/legal/termsofuse">Terms of Use</a></li>
+                        <li className ="content_li"><a  className="links" href="https://help.netflix.com/contactus">Contact Us</a></li>
                     </ul>
                 </div>
                 
