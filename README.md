1e8ff17b7f21fd0fed5026be761b92e1d1c55441
ritesh singh <rootritesh64@gmail.com>
2020-11-22 00:20:46 +0530

    originals added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Netflix Clone
+# Netflix Originals  Clone
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/nf_original/nf_original.js
+++ src/nf_original/nf_original.js
@@ -5,7 +5,7 @@ function Nf_ori(){
     return(
         <div className="nf-or">
 
-        <h1 className="h1_nf_or">Netflix Original</h1>
+        <h1 className="h1_nf_or">Netflix Originals</h1>
         <p className="para1">Netflix is the home of amazing original programming that you can’t find anywhere else. Movies, TV shows, specials and more, it’s all tailored specifically to you. <a className="watch_now" href="https://www.netflix.com/in/watch-free">Watch now for free</a></p>
         </div>
     );
